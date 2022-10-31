70e2a581729ef4725f1a4f2723f505c5a596a73d
thurain <thurainbowin.dev@gmail.com>
2022-10-31 21:16:25 +0630

    - start testing

---
+++ BurgerJoint/app/src/test/java/com/zg/burgerjoint/Testing.kt
@@ -0,0 +1,67 @@
+package com.zg.burgerjoint
+
+import android.widget.ImageView
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.zg.burgerjoint.data.model.BurgerModel
+import com.zg.burgerjoint.data.model.impls.BurgerModelImpl
+import com.zg.burgerjoint.data.model.impls.MockBurgerModelImpl
+import com.zg.burgerjoint.data.vos.BurgerVO
+import com.zg.burgerjoint.mvp.presenters.impls.MainPresenterImpl
+import com.zg.burgerjoint.mvp.views.MainView
+import io.mockk.MockKAnnotations
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.verify
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+@RunWith(AndroidJUnit4::class)
+@Config(manifest = Config.NONE)
+class Testing {
+
+    private lateinit var mPresenter: MainPresenterImpl
+
+    @RelaxedMockK
+    private lateinit var mView: MainView
+
+    private lateinit var mBurgerModel: BurgerModel
+
+    @Before
+    fun setUpPresenter() {
+        MockKAnnotations.init(this)
+
+        BurgerModelImpl.init(ApplicationProvider.getApplicationContext())
+        mBurgerModel = MockBurgerModelImpl
+
+        mPresenter = MainPresenterImpl()
+        mPresenter.initPresenter(mView)
+        mPresenter.mBurgerModel = this.mBurgerModel
+    }
+
+    @Test
+    fun onTapAddToCart_callAddBurgerToCart() {
+        val tappedBurger = BurgerVO()
+        tappedBurger.burgerId = 1
+        tappedBurger.burgerName = "Big Mac"
+        tappedBurger.burgerImageUrl = ""
+        tappedBurger.burgerDescription = "Big Mac Burger"
+
+        val imageView = ImageView(ApplicationProvider.getApplicationContext())
+
+        mPresenter.onTapAddToCart(tappedBurger, imageView)
+
+        verify {
+            mView.animateAddBurgerToCart(tappedBurger, imageView)
+        }
+    }
+
+    @Test
+    fun onTapCart_callNavigateToCartScreen() {
+        mPresenter.onTapCart()
+        verify {
+            mView.navigateToCartScreen()
+        }
+    }
+}
